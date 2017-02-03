18224d2e9f6d69fdb2d239bc59c78f9b936bf1de
pandabrand <pandabrand@gmail.com>
2017-02-03 10:35:43 -0600

    added favicons

--- web/app/themes/sage/templates/head.php
+++ web/app/themes/sage/templates/head.php
@@ -2,5 +2,10 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/apple-touch-icon.png" dochead="1">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/favicon-32x32.png" sizes="32x32" dochead="1">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/favicon-16x16.png" sizes="16x16" dochead="1">
+  <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/manifest.json" dochead="1">
+  <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/safari-pinned-tab.svg" color="#5bbad5" dochead="1">
   <?php wp_head(); ?>
 </head>
